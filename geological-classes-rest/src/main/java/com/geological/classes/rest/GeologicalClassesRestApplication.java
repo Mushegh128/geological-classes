88efc42142a5cf1b782dfe6e8cc069c1b64e0a97
Mushegh128 <thesoceco@gmail.com>
2022-04-03 20:27:23 +0400

    create base/packages skeleton

---
+++ geological-classes-rest/src/main/java/com/geological/classes/rest/GeologicalClassesRestApplication.java
@@ -0,0 +1,13 @@
+package com.geological.classes.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeologicalClassesRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeologicalClassesRestApplication.class, args);
+    }
+
+}
---
+++ geological-classes-rest/src/test/java/com/geological/classes/rest/GeologicalClassesRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geological.classes.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeologicalClassesRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
